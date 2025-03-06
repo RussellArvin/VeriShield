d86d7ea020c2589977426047b1f3b3e1538edd2f
Russell Arvin <56810871+RussellArvin@users.noreply.github.com>
2025-03-06 18:02:38 +0800

    chore: update landing

--- web-app/src/server/constants/APP_ROUTES.ts
+++ web-app/src/server/constants/APP_ROUTES.ts
@@ -1,6 +1,9 @@
 const APP_ROUTES = {
-    LANDING: '/test',
-    CLERK_WEBHOOK:"/api/clerk"
+    LANDING: '/',
+    CLERK_WEBHOOK:"/api/clerk",
+    APP:{
+        HOME:'/app'
+    }
 }
 
 export default APP_ROUTES;
