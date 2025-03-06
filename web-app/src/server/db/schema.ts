9f1a97717a34b9e0f0587a11776f988faba42890
Russell Arvin <56810871+RussellArvin@users.noreply.github.com>
2025-03-06 22:59:24 +0800

    feat: updated schema

--- web-app/src/server/db/schema.ts
+++ web-app/src/server/db/schema.ts
@@ -1,5 +1,6 @@
 import { sql } from "drizzle-orm";
 import {
+  PgTable,
   pgTable,
   text,
   timestamp,
@@ -21,3 +22,43 @@ export const userSchema = pgTable(
     .notNull(),
   }
 );
+
+export const threats = pgTable(
+  "threats",
+  {
+    id: uuid("id").primaryKey(),
+    userId: text("user_id").notNull().references(() => userSchema.id),
+    description: text("description").notNull(),
+    sourceUrl: text("source_url").notNull(),
+    source: text("source").notNull(),
+    createdAt: timestamp("created_at")
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    status: text("status").notNull(),
+  });
+  
+export const threatMedia = pgTable(
+  "threat_media",
+  {
+    id: uuid("id").primaryKey(),
+    threatId: uuid("threat_id").notNull().references(() => threats.id),
+    mediaUrl: text("media_url").notNull(),
+    createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`).notNull(),
+  }
+)
+  
+ export const threatResponse = pgTable(
+  "threat_response",
+  {
+    id: uuid("id").primaryKey(),
+    threatId: uuid("threat_id").notNull().references(() => threats.id),
+    type: text("type").notNull(),
+    response: text("response").notNull(),
+    createdAt: timestamp("created_at")
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: timestamp("updatedAt")
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  }
+ )
