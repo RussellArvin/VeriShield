d076c718bb24de179eed6501f89e7ca4386358c0
Russell Arvin <56810871+RussellArvin@users.noreply.github.com>
2025-03-06 16:57:09 +0800

    fix: eslint

--- web-app/.eslintrc.cjs
+++ web-app/.eslintrc.cjs
@@ -1,6 +1,26 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
   parser: "@typescript-eslint/parser",
+  overrides: [
+    {
+      files: ['src/components/ui/**/*.tsx', 'src/components/ui/**/*.ts'],
+      rules: {
+        '@typescript-eslint/no-empty-interface': 'off',
+        '@typescript-eslint/prefer-nullish-coalescing': 'off',
+        // You can add more rules to ignore here if needed
+      },
+    },
+    {
+      files:['src/tests/**/*.ts'],
+      rules: {
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+        '@typescript-eslint/unbound-method': 'off',
+        '@typescript-eslint/consistent-type-imports': 'off',
+        '@typescript-eslint/no-unused-vars': 'off'
+      }
+    }
+  ],
   parserOptions: {
     project: true,
   },
